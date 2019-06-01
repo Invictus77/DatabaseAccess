d0d23aeb1ca7697ed7968b984300c09d078c7c0b
Wolfgang Bräu <BRWO@auris-consult.at>
2019-06-01 15:26:39 +0200

    added DatabasekeyAttribute.

---
+++ DatabaseAccess/Attributes/DatabaseKeyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseAccess.Attributes
+{
+    /// <summary>
+    /// Represents a key field in the database.
+    /// </summary>
+    public class DatabaseKeyAttribute : Attribute
+    {
+    }
+}
